444f05793faa469495712f4acd9ab9937855d190
Jae Kyung Lee <ocooljay@naver.com>
2016-06-29 17:53:16 +0900

    add

--- Torch/models.py
+++ Torch/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Project(models.Model):
+    language = models.CharField(max_length=250)
+    projects = models.CharField(max_length=250)
+
+
+    def __str__(self):
+        return self.language + ' | ' + self.projects
+
+class ReportCard(models.Model):
+    language = models.ForeignKey(Project, on_delete=models.CASCADE)
