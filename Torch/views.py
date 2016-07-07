d5b48b33c57fda4355bcedc8ab2834b542181dc8
Eric <spb828@naver.com>
2016-07-07 15:04:32 +0900

    sign_up page restore

--- Torch/views.py
+++ Torch/views.py
@@ -7,6 +7,7 @@ from django.core.urlresolvers import reverse
 from django.template import loader
 from django.http import HttpResponse
 from .models import Project
+from users.models import User
 
 from .models import LoadFile
 
