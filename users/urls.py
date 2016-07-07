ac3c9e69a07a2f0da70df5a3d30c1557bd6f5f7a
Eric <spb828@naver.com>
2016-07-07 14:42:17 +0900

    add registration

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     # url(r'^$', 'django.contrib.auth.views.login'),
     # url(r'^logout/$', logout_page),
     # url(r'^accounts/login/$', 'django.contrib.auth.views.login'),  # If user is not login it will redirect to login page
-    url(r'^sign_up/$', sign_up),
+    # url(r'^sign_up/$', sign_up),
     url(r'^success/$', register_success),
     # url(r'^home/$', home),
 ]
